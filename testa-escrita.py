499beefcacf45d7e628a3d626085f53ce402e2d7
Felipe <felipemacedo986@gmail.com>
2022-11-10 20:54:15 -0300

    como a escrita no python funciona

--- testa-escrita.py
+++ testa-escrita.py
@@ -1,6 +1,16 @@
-arquivo_contatos = open('dados/contatos-escrita.csv', encoding='latin-1', mode='a') #estamos indicando o modo de escrita
+arquivo_contatos = open('dados/contatos-escrita.csv', encoding='latin-1', mode='w') #estamos indicando o modo de escrita
 #(só que sem sobrescrever o arquivo). Por default, o python abre arquivos no modo leitura (r)
 
-contato = '11,Carol,carol@carol.com.br\n'
-arquivo_contatos.write(contato)
-#nesse caso, se o arquivo não existir, o python cria um
+contatos = ['11,Carol,carol@carol.com.br\n',
+            '12,Ana,ana@ana.com.br\n',
+            '13,Tais,tais@tais.com.br\n',
+            '14,Felipe,felipe@felipe.com.br\n']
+
+for contato in contatos:
+    arquivo_contatos.write(contato)
+
+#arquivo_contatos.close() precisamos indicar que não estamos mais trabalhando com o arquivo para ele aí sim inserir o texto
+
+arquivo_contatos.flush() #já aqui forçamos ele a inserir os elementos mesmo com o arquivo aberto
+
+input('Pressione <Enter> para encerrar o programa')
